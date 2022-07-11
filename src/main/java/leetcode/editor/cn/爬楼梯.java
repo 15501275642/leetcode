8fc72ae5b5230aebd2d45ae06fc29dd590b4ee30
wangyanji <jijindong@abakusglobal.com>
2022-07-11 16:48:48 +0800

    创建

---
+++ src/main/java/leetcode/editor/cn/爬楼梯.java
@@ -0,0 +1,59 @@
+//假设你正在爬楼梯。需要 n 阶你才能到达楼顶。 
+//
+// 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？ 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：n = 2
+//输出：2
+//解释：有两种方法可以爬到楼顶。
+//1. 1 阶 + 1 阶
+//2. 2 阶 
+//
+// 示例 2： 
+//
+// 
+//输入：n = 3
+//输出：3
+//解释：有三种方法可以爬到楼顶。
+//1. 1 阶 + 1 阶 + 1 阶
+//2. 1 阶 + 2 阶
+//3. 2 阶 + 1 阶
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= n <= 45 
+// 
+// Related Topics 记忆化搜索 数学 动态规划 👍 2514 👎 0
+
+package leetcode.editor.cn;
+
+/**
+ * 爬楼梯
+ *
+ * @author wangyanji
+ * @date 2022-07-08 15:55:19
+ */
+public class 爬楼梯{
+    public static void main(String[] args) {
+        Solution solution = new 爬楼梯().new Solution();
+         
+    }
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int climbStairs(int n) {
+        return 2;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
+
+
