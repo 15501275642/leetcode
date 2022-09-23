d076cca8d482d23d82dd4c25d7694cc5421ce765
wangyanji <761255104@qq.com>
2022-09-23 16:38:57 +0800

    gitignore

---
+++ src/main/java/leetcode/editor/cn/三除数.java
@@ -0,0 +1,51 @@
+//给你一个整数 n 。如果 n 恰好有三个正除数 ，返回 true ；否则，返回 false 。 
+//
+// 如果存在整数 k ，满足 n = k * m ，那么整数 m 就是 n 的一个 除数 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：n = 2
+//输出：false
+//解释：2 只有两个除数：1 和 2 。 
+//
+// 示例 2： 
+//
+// 输入：n = 4
+//输出：true
+//解释：4 有三个除数：1、2 和 4 。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= n <= 10⁴ 
+// 
+//
+// Related Topics 数学 👍 19 👎 0
+
+package leetcode.editor.cn;
+
+/**
+ * 三除数
+ *
+ * @author wangyanji
+ * @date 2022-09-23 16:34:07
+ */
+public class 三除数{
+    public static void main(String[] args) {
+        Solution solution = new 三除数().new Solution();
+         
+    }
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean isThree(int n) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
