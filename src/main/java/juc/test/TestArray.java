2e3d1bf2f5944b147c1581da41f8379a2dfd8bd1
wangyanji <761255104@qq.com>
2022-12-06 15:39:24 +0800

    测试提交

--- src/main/java/juc/test/TestArray.java
+++ src/main/java/juc/test/TestArray.java
@@ -1,10 +1,25 @@
 package juc.test;
 
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class TestArray {
     public static void main(String[] args) {
+        Long start = Long.valueOf(1);
+        Long limit = Long.valueOf(3);
+        Set<Long> filterUserIdSetx = new HashSet<>();
+        filterUserIdSetx.add(Long.valueOf(1));
+        filterUserIdSetx.add(Long.valueOf(2));
+        filterUserIdSetx.add(Long.valueOf(3));
+        filterUserIdSetx.add(Long.valueOf(4));
+        filterUserIdSetx.add(Long.valueOf(5));
+         //Comparator.comparing((Function<Object, Object>) o -> o);
 
+        ((Set)filterUserIdSetx).stream().sorted(
+
+
+        ).skip((long)start).limit((long)limit).collect(Collectors.toList());
         List<String> strings = Arrays.asList("15501275642", "18801275642");
 
         Set<String> resSet = new HashSet<>(strings);
